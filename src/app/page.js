5c3196ed8e2b3b999f40983f6e0ce6a0418f0da2
Muhammad Tahir <muhammadtahirbharchoond@gmail.com>
2024-01-04 16:23:18 +0500

    footer, contact us and about page
    created footer section, contact us and also about us page

--- src/app/page.js
+++ src/app/page.js
@@ -10,17 +10,33 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
+      {/* About us */}
       <section className="text-center my-16">
         <SectionHeaders 
           subHeader={'Our story'}
           mainHeader={'About us'}
         />
-        <div className="text-gray-500 max-w-2xl mx-auto mt-4 flex flex-col gap-4">
-          <p>Lorem ipsum dolor sit amet, cobse</p>
-          <p></p>
-          <p></p>
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
+          <p>Thank you once again for considering my application. I am eager to contribute to the success of Sapphire Consulting Services and look forward to the possibility of joining your team.</p>
+          <p>Thank you once again for considering my application. I am eager to contribute to the success of Sapphire Consulting Services and look forward to the possibility of joining your team.</p>
+          <p>Thank you once again for considering my application. I am eager to contribute to the success of Sapphire Consulting Services.</p>
         </div>
       </section>
+
+      {/* Contact us */}
+      <section className="text-center my-8">
+        <SectionHeaders 
+          subHeader={'Don\'t hesitate' }
+          mainHeader={'Contact us'}
+        />
+        <div className="mt-8 flex flex-col gap-4">
+          <a className="text-4xl underline text-gray-500" href="tel:+921234567891">+921234567891</a>
+          <a className="text-4xl underline text-gray-500" href="email:foodbizz@gmail.com">foodbizz@gmail.com</a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt16">
+          &copy; 2024 All rights reserved to FOOD BIZZ
+      </footer>
     </>
   );
 }
