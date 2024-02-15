1e4201633e48b10118552dd518787f590b6a7b22
Muhammad Tahir <muhammadtahirbharchoond@gmail.com>
2024-02-15 15:35:19 +0500

    profile page
    added re-name functionality to profile page.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,7 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 
-
-const handler = NextAuth({
+export const authOptions = {
   secret: process.env.SECRET,
   providers: [
     CredentialsProvider({
@@ -33,6 +32,8 @@ const handler = NextAuth({
       },
     }),
   ],
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,10 +2,28 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function ProfilePage() {
   const session = useSession();
+  const [userName, setUserName] = useState('');
   const { status } = session;
+
+  useEffect(()=>{
+    if(status === 'authenticated'){
+      setUserName(session.data.user.name)
+    }
+  },[session, status]);
+
+  async function handleProfileInfoUpdate(ev) {
+    ev.preventDefault();
+    const response = await fetch("/api/profile", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: userName }),
+    });
+  }
+
   if (status === "loading") {
     return "lodaing...";
   }
@@ -16,17 +34,37 @@ export default function ProfilePage() {
   return (
     <section className="mt-8 ">
       <h1 className="text-center text-primary text-4xl mb-4">Profile</h1>
-      <form className="max-w-md mx-auto">
+      <p>this is {userName}</p>
+      <div className="max-w-md mx-auto">
         <div className="flex gap-2 items-center">
-            <div className="bg-gray-300 p-2 rounded-lg">
-            <Image className="rounded-lg" src={userImage} width={80} height={80} alt="avatar" />
+          <div>
+            <div className="p-2 rounded-lg relative bg-gray-400">
+              <Image
+                className="rounded-lg w-full h-full mb-1"
+                src={userImage}
+                width={250}
+                height={250}
+                alt="avatar"
+              />
+              <button type="button">Edit</button>
             </div>
-            <div className="grow">
-                <input type="text" placeholder="First and last name"/>
-                <button type="submit">Save</button>
-            </div>
+          </div>
+          <form className="grow" onSubmit={handleProfileInfoUpdate}>
+            <input
+              type="text"
+              placeholder="First and last name"
+              value={userName}
+              onChange={(ev) => setUserName(ev.target.value)}
+            />
+            <input
+              type="email"
+              disabled={true}
+              value={session.data.user.email}
+            />
+            <button type="submit">Save</button>
+          </form>
         </div>
-      </form>
+      </div>
     </section>
   );
 }
